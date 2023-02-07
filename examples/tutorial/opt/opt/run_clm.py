777a5429fed8b0198b9130fc872af18873abd2a3
George Ang <gnap.an@gmail.com>
2023-02-07 16:45:45 +0800

    auto device placement.

--- examples/tutorial/opt/opt/run_clm.py
+++ examples/tutorial/opt/opt/run_clm.py
@@ -333,7 +333,7 @@ def main():
     disable_existing_loggers()
     colossalai.launch_from_torch(config=dict(
         zero = dict(model_config=dict(shard_strategy=TensorShardStrategy(),
-                              tensor_placement_policy="cpu",
+                              tensor_placement_policy="auto",
                               reuse_fp16_shard=True),
             optimizer_config=dict(gpu_margin_mem_ratio=0.8, initial_scale=16384)),
         # fp16 = dict(mode=AMP_TYPE.NAIVE),
